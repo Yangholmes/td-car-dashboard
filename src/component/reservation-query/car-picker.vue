8382fe83db80424004fad43f50bc16163dc36366
yangholmes <yangholmes@126.com>
2017-06-04 01:27:27 +0800

    building frame of car-picker.

---
+++ src/component/reservation-query/car-picker.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="car-picker">
+    <ul class="car-picker-list popup">
+      <li class="car-picker-list-item">
+       <div class="car-picker-list-item-img">
+         <img src="http://www.gdrtc.org/car/img/cars-pics/car1490331925.jpg">
+       </div>
+       <div class="car-picker-list-item-text">白色奥德赛</div>
+     </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['cars'],
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    this.$http.get('http://www.gdrtc.org/car/server/reservation/application-init.php',{responseType: 'json'}).then(response => {
+      console.log(response.data);
+    }, response => {
+      console.log(response);
+    });
+  }
+}
+</script>
+
+<style>
+.car-picker * {
+  background-color: white;
+  margin: 0; padding: 0;
+}
+ul.car-picker-list {
+  display: block;
+  z-index: 100;
+  position: fixed;
+  top: 2em;
+  left: 2.5em;
+
+  width: calc( 100% - 5em );
+  max-height: calc( 100% - 5em);
+
+  overflow-y: scroll;
+  box-shadow: 1px 0 5px #FFF;
+  border-radius: 3px;
+}
+ul.car-picker-list li.car-picker-list-item {
+  display: flex;
+}
+ul.car-picker-list li.car-picker-list-item div.car-picker-list-item-img {
+  display: inline-block;
+  width: 64px; height: 64px;
+  margin-right: .5em;
+}
+ul.car-picker-list li.car-picker-list-item div.car-picker-list-item-img img {
+  width: 64px; height: 64px;
+  border-radius: 50%;
+}
+ul.car-picker-list li.car-picker-list-item div.car-picker-list-item-text {
+  display: inline-block;
+  line-height: 64px;
+  width: calc( 100% - 64px );
+  margin: 0 .5em
+}
+</style>
--- src/component/reservation-query/index.vue
+++ src/component/reservation-query/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="reservation-query">
     <div class="query-key-form">
-      <!-- <car-picker></car-picker> -->
-      
-      </div>
+      <car-picker :car="car"></car-picker>
+      <!-- <datetime-picker></datetime-picker> -->
     </div>
   </div>
 </template>
 
 <script>
+import carPicker from './car-picker.vue'
+import datetimePicker from './datetime-picker.vue'
 export default {
+  components: {
+    // carPicker: (resolve) => require(['./car-picker.vue'], resolve) // 异步加载异常
+    carPicker
+  },
   data () {
     return {
-      date: null,
+      car: [],
     }
   },
   mounted () {
